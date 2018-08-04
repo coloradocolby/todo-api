dab1175effa281c67cd592aacbc4d08e940a7a67
Colby Thomas <coloradocolby@gmail.com>
2018-08-04 11:49:52 -0500

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); // we can now send json to our express app
 
@@ -47,8 +48,8 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
 module.exports = { app };
