5b3ac32d46ffcc1b82956528919a7f10d5b9951b
Colby Thomas <coloradocolby@gmail.com>
2018-08-04 11:14:06 -0500

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -16,65 +19,88 @@ beforeEach((done) => {
   }).then(() => done());
 });
 
-describe('Server tests', () => {
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
 
-  describe('POST /todos', () => {
-    it('should create a new todo', (done) => {
-      var text = 'Test todo text';
+    request(app)
+      .post('/todos')
+      .send({ text })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
 
-      request(app)
-        .post('/todos')
-        .send({ text })
-        .expect(200)
-        .expect((res) => {
-          expect(res.body.text).toBe(text);
-        })
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-
-          Todo.find({ text }).then((todos) => {
-            expect(todos.length).toBe(1);
-            expect(todos[0].text).toBe(text);
-            done();
-            // if there are any errors in the callback above, catch them
-          }).catch((e) => done(e));
-        })
-    });
-
-    it('should not create todo with invalid body data', (done) => {
-      request(app)
-        .post('/todos')
-        .send({})
-        .expect(400)
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-
-          Todo.find().then((todos) => {
-            expect(todos.length).toBe(2);
-            done();
-          }).catch((e) => done(e));
-        })
-    });
-  });
-
-  describe('GET /todos', () => {
-    it('should fetch all todos', (done) => {
-      request(app)
-        .get('/todos')
-        .expect(200)
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-
-          expect(res.body.todos.length).toBe(2);
+        Todo.find({ text }).then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
           done();
-        })
-    });
+          // if there are any errors in the callback above, catch them
+        }).catch((e) => done(e));
+      })
   });
 
+  it('should not create todo with invalid body data', (done) => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(2);
+          done();
+        }).catch((e) => done(e));
+      })
+  });
 });
+
+describe('GET /todos', () => {
+  it('should fetch all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.body.todos.length).toBe(2);
+        done();
+      });
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    request(app)
+      .get(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/123abc') // 123abc is not a valid ObjectID
+      .expect(404)
+      .end(done);
+  });
+});
+
