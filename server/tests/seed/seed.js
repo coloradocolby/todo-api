a0df5b8d7bd34258dd65f513fd06548ab1121019
Colby Thomas <coloradocolby@gmail.com>
2018-08-05 15:38:36 -0500

    Add tests for GET /users/me and POST /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,51 @@
+const { ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [{
+  // valid user with valid auth
+  _id: userOneId,
+  email: 'colby@example.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'michelle@example.com',
+  password: 'userTwoPass'
+}];
+
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
+}];
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    // Promise.all lets you wait for all promises in array to resolve
+    return Promise.all([userOne, userTwo]);
+  }).then(() => done());
+};
+
+const populateTodos = (done) => {
+  // reset all todos after each test
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+};
+
+module.exports = { todos, populateTodos, users, populateUsers };
